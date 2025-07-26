eef1e9bb5f74166e962e0d5fa96bc0efdd4615a5
Prakhar Misra <96350303+PaMLaM876@users.noreply.github.com>
2025-07-26 15:32:44 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+//console.log("Hello World");
+let firstName= "Prakhar";
+let lastName= "Misra";
+document.getElementById("myH1").textContent=`My first name is ${firstName}`;
+document.getElementById("myP").textContent=`My last name is ${lastName}`;
